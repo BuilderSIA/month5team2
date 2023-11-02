eab329fab485028f09b69936da54234107d65d14
Muslimbee <shaymardonovmuslim27@gmail.com>
2023-11-02 18:43:13 +0500

    HTML CSS SASS finished

---
+++ js/muslim.data.js/data.js
@@ -0,0 +1,53 @@
+let allProducts = [
+    {
+      productId: 1,
+      productImg: "/images/image-1.png",
+      productName: "Sut",
+      productPrice: 139,
+      productDesc: "lorem ipsum dolor sit amet dolor sit amet dolor sit amet dolor sit",
+      productIsSelected: false,
+    },
+  
+    {
+      productId: 2,
+      productImg: "/images/image-2.png",
+      productName: "Sut",
+      productPrice: 13,
+      productDesc: "lorem ipsum dolor sit amet dolor sit amet dolor sit amet dolor sit",
+      productIsSelected: false,
+    },
+    {
+      productId: 3,
+      productImg: "/images/image-3.png",
+      productName: "Maloko",
+      productPrice: 1339,
+      productDesc: "lorem ipsum dolor sit amet dolor sit amet dolor sit amet dolor sit",
+      productIsSelected: false,
+    },
+    {
+      productId: 4,
+      productImg: "/images/image-4.png",
+      productName: "Maslo",
+      productPrice: 13923,
+      productDesc: "lorem ipsum dolor sit amet dolor sit amet dolor sit amet dolor sit",
+      productIsSelected: false,
+    },
+    {
+      productId: 5,
+      productImg: "/images/image-5.png",
+      productName: "Yogurt",
+      productPrice: 139453,
+      productIsSelected: false,
+      productDesc: "lorem ipsum dolor sit amet dolor sit amet dolor sit amet dolor sit",
+  
+    },
+    {
+      productId: 6,
+      productImg: "/images/image-6.png",
+      productName: "Yogurt",
+      productPrice: 139675,
+      productDesc: "lorem ipsum dolor sit amet dolor sit amet dolor sit amet dolor sit",
+      productIsSelected: false,
+    },
+  ];
+  
