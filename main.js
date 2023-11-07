c662c3c05242672e642ab550e92e968bfe49e2ca
BuilderSIA <lordallplay@gmail.com>
2023-11-07 16:47:31 +0500

    to images given id must loop the array

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const url = 'https://fakestoreapi.com/products';
 
+const prodImg = document.querySelectorAll('#prodImg');
+
+
+
 fetch(url)
-.then((res)=>res.json())
-.then((data)=>{
-    console.log(data);
+.then((res) => res.json())
+.then((data) => {
+    let itemImg = ''
+    // console.log(data);
+    let products = data.map((item) => {
+        // console.log(item);
+        
+        itemImg = item.image;
+        // 
+        
+    })
+    for(let i = 0; i<prodImg.length;i++){
+             
+        prodImg[i].setAttribute('src',itemImg);
+    }
 })
