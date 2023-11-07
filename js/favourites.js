920e380925efcf19e758d6b8444603f08323edb0
Muh1isa <lisaa27.a6@gmail.com>
2023-11-07 19:19:04 +0500

    some codes have been changed

--- js/favourites.js
+++ js/favourites.js
@@ -1,38 +1,63 @@
-let inp = document.getElementById("inp");
-let elParent = document.querySelector(".card--body-child-1");
+let url = "https://fakestoreapi.com/products";
 
-function renderData(allData) {
-  allData.map((item) => {
-    let elCard = document.createElement("div");
-    elCard.setAttribute("class", "card-child");
-    elCard.innerHTML = `
-            <i class="bx bx-heart"></i>
-            <img class="tavar" src="${item.productImg}" alt="" />
-            <h2>${item.productName}</h2>
-            <h4>${item.productPrice} ₽</h4>
-            <p>${item.productDesc}</p>
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    let inp = document.getElementById("inp");
+    let elParent = document.querySelector(".card--body-child-1");
+
+    function renderData(data) {
+      data.map((item) => {
+        let elCard = document.createElement("div");
+        elCard.setAttribute("class", "card-child");
+        elCard.innerHTML = `
+        <i class="fa-solid fa-heart" style="color: #000000;"></i></i>
+            <img class="tavar" src="${item.image}" alt="" />
+            <h2>${item.title}</h2>
+            <h4>${item.price} ₽</h4>
            
             <button class="child-btn">В корзину</button>
         `;
-    elParent.appendChild(elCard);
-  });
-}
-renderData(allProducts);
-let arr = [];
-
-inp.addEventListener('keyup', () => {
-  allProducts.map((element) => {
-    if (element.productName.toLowerCase() === inp.value || element.productName === inp.value || element.productName.toUpperCase() === inp.value) {
-      arr.push(element);
+        elParent.appendChild(elCard);
+      });
     }
-  });
-  console.log(arr);
-  if (arr.length > 0) {
-    elParent.innerHTML = "";
 
-    renderData(arr);
-  } else {
-    elParent.innerHTML = "";
-    renderData(allProducts);
-  }
-});
+
+    renderData(data);
+    let arr = [];
+
+
+
+    inp.addEventListener('input', () => {
+      data.map((element) => {
+        if (element.title.toLowerCase() === inp.value || element.title === inp.value || element.title.toUpperCase() === inp.value) {
+          arr.push(element);
+        }
+      });
+      console.log(arr);
+      if (arr.length > 0) {
+        elParent.innerHTML = "";
+    
+        renderData(arr);
+      } else {
+        elParent.innerHTML = "";
+        renderData(data);
+      }
+    });
+  })
+
+
+
+  const heart = document.querySelector(".card--body-child-1")
+  let a = JSON.parse(localStorage.getItem("liked")) || []
+  heart.addEventListener("click", (e) =>{
+      let likedProduct = e.target.closest(".fa-heart")
+      if(likedProduct){
+          if(likedProduct.toggleAttribute("true")){
+              let s = likedProduct.style = `color:red` ;
+              a.push(s)
+              localStorage.setItem("liked", JSON.stringify(a))
+          } else{
+              likedProduct.style = `color:#000`
+          }
+      }})
--- main.js
+++ main.js
@@ -7,17 +7,8 @@ const prodImg = document.querySelectorAll('#prodImg');
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
-    let itemImg = ''
     // console.log(data);
-    let products = data.map((item) => {
-        // console.log(item);
-        
-        itemImg = item.image;
-        // 
-        
-    })
-    for(let i = 0; i<prodImg.length;i++){
-             
-        prodImg[i].setAttribute('src',itemImg);
+    for(let i = 0; i< data.length;i++){
+        prodImg[i].setAttribute('src', data[i].image);
     }
 })
