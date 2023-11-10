a023311cf1233a789b2facda128916fc838104f3
BuilderSIA <lordallplay@gmail.com>
2023-11-10 13:49:34 +0500

    import liked animation

--- main.js
+++ main.js
@@ -5,14 +5,10 @@ const titleP = document.querySelectorAll('.text_p');
 const toCart = document.querySelectorAll('.menu_btn');
 const heart = document.querySelectorAll('#heart');
 
-const redhrt = "fa-solid fa-heart";
-const whitehrt = "fa-regular fa-heart";
 let p = 0;
-
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
-    // console.log(data);
     let x = 0;
     let heartType = heart[x].getAttribute('class');
     data.map((item)=>{
@@ -22,45 +18,34 @@ fetch(url)
                 heart[x].setAttribute('class','fa-solid fa-heart')
             }  
         })
-        // console.log(item);
         prodImg[x].setAttribute('src',String(item.image));
         titleP[x].textContent = item.title;
         heart[x].addEventListener('click',(icon)=>{
-            
-            setFav(item);
-            
-            
-            
-            
-            
-            // favs[x].map((local)=>{
-            //     console.log(local);
-            // })
-            // if(){
-            //     icon.srcElement.className = 'fa-solid fa-heart'
-            // }
+            if( icon.target.className == 'fa-regular fa-heart'){
+                setFav(item);
+            }else if(icon.target.className == 'fa-solid fa-heart'){
+                favs.map((elem)=>{
+                    p = elem.liked.id;
+                    if(p == item.id){
+                    let z = favs.indexOf(elem);
+                    unsetFav(item,z)    
+                    }
+                })
+            }
             location.reload(true)
         })
-        
-        
-        
         x += 1;
-        
     })
-    
-    
 })
 
 
 function getFav(){
     return localStorage.getItem('liked') ? JSON.parse(localStorage.getItem('liked')) : [];
 }
-
 function setFav(liked){
     let arrFav = 0;
     let item = {liked}
     let items = getFav('liked');
-    // console.log(items);
     items.map((e)=>{
         if(e.liked.id == item.liked.id){
             arrFav += 1;
@@ -71,23 +56,21 @@ function setFav(liked){
     }
     localStorage.setItem('liked',JSON.stringify(items))
 }
-// function unsetFav(liked){
-//     let arrFav = 0;
-//     let item = {liked}
-//     let items = getFav('liked');
-//     localStorage.removeItem('liked');
-//     items.map((e)=>{
-//         if(e.liked.id == item.liked.id){
-//             arrFav += 1;
-//         }
-//     })
-//     if(arrFav == 0){
-//         items.splice(p,1);
-//         console.log(items);
-//     }
-//     localStorage.setItem('liked',JSON.stringify(items))
-    
-// }
+function unsetFav(liked,z){
+    let arrFav = 0;
+    let item = {liked}
+    let items = getFav();
+    localStorage.removeItem('liked');
+    items.map((e)=>{
+        if(e.liked.id == item.liked.id){
+            arrFav += 1;
+        }
+    })
+    if(arrFav != 0){   
+        items.splice(z,1);
+    }
+    localStorage.setItem('liked',JSON.stringify(items))
+}
 
 
 
